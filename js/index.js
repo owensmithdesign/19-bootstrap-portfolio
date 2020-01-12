50b6d7135b3db27813c5272788d91dc130402f53
owensmithdesign <smith.owen121@gmail.com>
2020-01-12 12:04:31 -0500

    start changes on 1.12
    page before interactive elements

---
+++ js/index.js
@@ -0,0 +1,8 @@
+$(".nav-link").on("click", function(){ 
+    $()
+})
+
+$('#example-one a').click(function(e) {
+    $('#example-one a').removeClass('current_page_item');
+    $(this).addClass('current_page_item');
+});
