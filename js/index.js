121c10302ea7b56416821c71c6331db07f2bdc31
owensmithdesign <smith.owen121@gmail.com>
2020-01-14 08:36:20 -0500

    done
    done

--- js/index.js
+++ js/index.js
@@ -6,3 +6,9 @@ $('#example-one a').click(function(e) {
     $('#example-one a').removeClass('current_page_item');
     $(this).addClass('current_page_item');
 });
+
+$(".scrollButton").click(function() {
+    $(index).animate({
+        scrollBottom: $("#caseStudyLocation").offset().top
+    }, 2000)
+});
